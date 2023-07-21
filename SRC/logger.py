6c1d4ffbcd587d2de9be08812ee6bcd37dff190c
tejas patil <patiltejas12051@gmail.com>
2023-07-21 20:07:37 +0530

    exception and logging file is added

---
+++ SRC/logger.py
@@ -0,0 +1,17 @@
+import os
+import logging
+from datetime import datetime
+
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
+logs_path = os.path.join(os.getcwd(),"logs", LOG_FILE)   #set the path for log file
+os.makedirs(logs_path,exist_ok=True)   #It says even though there is file keep updating that folder,file
+
+LOG_FILE_PATH = os.path.join(logs_path,LOG_FILE)
+
+#for saving the file
+logging.basicConfig(
+    filename = LOG_FILE_PATH,
+    format = "[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level = logging.INFO(),  # info is used to pring the messages
+
+)
